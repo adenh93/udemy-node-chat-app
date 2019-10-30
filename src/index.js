1fea83045094a469212bdd65bb8a55e9493f1b3c
Aden Herold <aden.herold1@gmail.com>
2019-10-30 20:46:10 +0930

    Added socket.io dependency to package.json. Added basic support for socket.io on server and clinet

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 require("dotenv").config();
 const path = require("path");
+const http = require("http");
 const express = require("express");
-
-const publicDir = path.join(__dirname, "../public");
+const socketio = require("socket.io");
 
 const port = process.env.PORT || 3000;
+const publicDir = path.join(__dirname, "../public");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(publicDir));
 
-app.listen(port, () => {
+io.on("connection", () => {
+  console.log("New Websocket connection");
+});
+
+server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
